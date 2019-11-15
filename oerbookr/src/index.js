41b78deb66369216011a83afce46d23b4c84abd7
lesley <lesleybanadzem2017@gmail.com>
2019-11-15 11:45:05 -0800

    Imported reducers to the index.js and created the store and made it availableto the entire app

--- oerbookr/src/index.js
+++ oerbookr/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, combineReducers} from 'redux'
+import {Provider} from 'react-redux'
+import { BrowserRouter  as Router} from 'react-router-dom';
+
+
 import './index.css';
 import App from './App';
 
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter  as Router} from 'react-router-dom';
+import {reducer as fetchIdReducers} from './reducer/login'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+
+const rootReducers = combineReducers({
+    fetchIdReducers: fetchIdReducers
+});
+
+const store = createStore(rootReducers);
+
+ReactDOM.render(<Provider store={ store }><Router><App /></Router></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
